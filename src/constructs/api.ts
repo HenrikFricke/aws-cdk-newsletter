f8d4555ecb5ec91279a9589eb6e754ae796800a1
Henrik Fricke <henrikfricke@web.de>
2021-11-06 16:21:53 +0100

    feat(api): request validator for subscribe/unsubscribe

--- src/constructs/api.ts
+++ src/constructs/api.ts
@@ -11,12 +11,41 @@ export class Api extends cdk.Construct {
   constructor(scope: cdk.Construct, id: string, props: ApiProps) {
     super(scope, id);
 
+    // IAM roles for AWS integrations
     const confirmIntegrationRole = new iam.Role(this, 'ConfirmIntegrationRole', {
       assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
     });
     props.stateMachine.grantTaskResponse(confirmIntegrationRole);
 
+    const subscribeOrUnsubscribeIntegrationRole = new iam.Role(this, 'SubscribeOrUnsubscribeIntegrationRole', {
+      assumedBy: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+    });
+    props.stateMachine.grantStartExecution(subscribeOrUnsubscribeIntegrationRole);
+
+    // API definition
     const api = new apigateway.RestApi(this, 'NewsletterApi');
+
+    const requestValidator = new apigateway.RequestValidator(this, 'SubscribeValidator', {
+      restApi: api,
+      validateRequestParameters: true,
+      validateRequestBody: true,
+    });
+
+    const subscriptionInputModel = api.addModel('SubscriptionInputModel', {
+      contentType: 'application/json',
+      modelName: 'SubscriptionInputModel',
+      schema: {
+        schema: apigateway.JsonSchemaVersion.DRAFT4,
+        title: 'subscriptionInputModel',
+        type: apigateway.JsonSchemaType.OBJECT,
+        properties: {
+          email: {
+            type: apigateway.JsonSchemaType.STRING,
+            format: 'email',
+          },
+        },
+      },
+    });
 
     api.root.addResource('confirm').addMethod(
       'POST',
@@ -29,7 +58,7 @@ export class Api extends cdk.Construct {
             {
               statusCode: '200',
               responseTemplates: {
-                'application/json': JSON.stringify({ done: true }),
+                'application/json': JSON.stringify({ ok: true }),
               },
             },
           ],
@@ -43,6 +72,80 @@ export class Api extends cdk.Construct {
       }),
       {
         methodResponses: [{ statusCode: '200' }],
+        requestValidator,
+        requestParameters: {
+          'method.request.querystring.token': true,
+        },
+      },
+    );
+
+    api.root.addResource('subscribe').addMethod(
+      'POST',
+      new apigateway.AwsIntegration({
+        service: 'states',
+        action: 'StartExecution',
+        options: {
+          credentialsRole: subscribeOrUnsubscribeIntegrationRole,
+          integrationResponses: [
+            {
+              statusCode: '200',
+              responseTemplates: {
+                'application/json': JSON.stringify({ ok: true }),
+              },
+            },
+          ],
+          requestTemplates: {
+            'application/json': JSON.stringify({
+              stateMachineArn: props.stateMachine.stateMachineArn,
+              input: JSON.stringify({
+                email: '$input.params(\'email\')',
+                type: 'SUBSCRIBE',
+              }),
+            }),
+          },
+        },
+      }),
+      {
+        methodResponses: [{ statusCode: '200' }],
+        requestValidator,
+        requestModels: {
+          'application/json': subscriptionInputModel,
+        },
+      },
+    );
+
+    api.root.addResource('unsubscribe').addMethod(
+      'POST',
+      new apigateway.AwsIntegration({
+        service: 'states',
+        action: 'StartExecution',
+        options: {
+          credentialsRole: subscribeOrUnsubscribeIntegrationRole,
+          integrationResponses: [
+            {
+              statusCode: '200',
+              responseTemplates: {
+                'application/json': JSON.stringify({ ok: true }),
+              },
+            },
+          ],
+          requestTemplates: {
+            'application/json': JSON.stringify({
+              stateMachineArn: props.stateMachine.stateMachineArn,
+              input: JSON.stringify({
+                email: '$input.params(\'email\')',
+                type: 'UNSUBSCRIBE',
+              }),
+            }),
+          },
+        },
+      }),
+      {
+        methodResponses: [{ statusCode: '200' }],
+        requestValidator,
+        requestModels: {
+          'application/json': subscriptionInputModel,
+        },
       },
     );
   }
