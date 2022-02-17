4cbb1eca15988ca6c8c851426c09d26652832c08
Henrik Fricke <henrikfricke@web.de>
2022-02-17 18:05:45 +0100

    fix: unit tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,11 +1,19 @@
 import '@aws-cdk/assert/jest';
-import { App } from '@aws-cdk/core';
-import { MyStack } from '../src/main';
+import { App, Duration } from 'aws-cdk-lib';
+import { NewsletterStack } from '../src/main';
 
 test('Snapshot', () => {
   const app = new App();
-  const stack = new MyStack(app, 'test');
+  const stack = new NewsletterStack(app, 'test', {
+    fromEmailAddress: 'henrikfricke@web.de',
+    confirmationLinkExpirationTime: Duration.days(1),
+    redirectUrls: {
+      subscribed: 'https://example.com/subscribed',
+      unsubscribed: 'https://example.com/unsubscribed',
+      expired: 'https://example.com/expired',
+      error: 'https://example.com/ohno',
+    },
+  });
 
-  expect(stack).not.toHaveResource('AWS::S3::Bucket');
   expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
 });
